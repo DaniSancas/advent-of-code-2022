3b0d37d947239ec7f4ddb1e9ecf6cbc560ee9aea
Dani Sancas <lord.sancas@gmail.com>
2022-12-04 19:41:30 +0100

    day3 part2

--- src/day3.rs
+++ src/day3.rs
@@ -6,17 +6,44 @@ const UPPERCASE_OFFSET: u32 = 26;
 
 #[aoc(day3, part1)]
 pub fn part1(input: &str) -> u32 {
-    get_rucksack_items(input)
+    get_rucksack_items_part1(input)
         .and_then(|items| get_total_priority_from_items(&items))
         .unwrap_or_default()
 }
 
-fn get_rucksack_items(text: &str) -> Result<Vec<char>> {
+#[aoc(day3, part2)]
+pub fn part2(input: &str) -> u32 {
+    get_rucksack_items_part2(input)
+        .and_then(|items| get_total_priority_from_items(&items))
+        .unwrap_or_default()
+}
+
+fn get_rucksack_items_part1(text: &str) -> Result<Vec<char>> {
     let mut rucksack_items: Vec<char> = Vec::new();
     for line in text.lines() {
         let (left, right) = line.split_at(line.len() / 2);
         let common_item: Result<char> = get_common_item_in_both_halves(left, right);
         rucksack_items.push(common_item?);
+    }
+
+    Ok(rucksack_items)
+}
+
+fn get_rucksack_items_part2(text: &str) -> Result<Vec<char>> {
+    let mut rucksack_items: Vec<char> = Vec::new();
+    let mut iterator = text.lines();
+    loop {
+        let first_option = iterator.next();
+        let second_option = iterator.next();
+        let third_option = iterator.next();
+        match (first_option, second_option, third_option) {
+            (Some(first), Some(second), Some(third)) => {
+                let common_item: Result<char> =
+                    get_common_item_in_3_rucksacks(first, second, third);
+                rucksack_items.push(common_item?);
+            }
+            (_, _, _) => break,
+        }
     }
 
     Ok(rucksack_items)
@@ -35,6 +62,27 @@ fn get_common_item_in_both_halves(left: &str, right: &str) -> Result<char> {
         "Not common character found in both halves: '{}' and '{}'",
         left,
         right
+    ))
+}
+
+fn get_common_item_in_3_rucksacks(first: &str, second: &str, third: &str) -> Result<char> {
+    for char_first in first.chars() {
+        for char_second in second.chars() {
+            if char_first.eq(&char_second) {
+                for char_third in third.chars() {
+                    if char_first.eq(&char_third) {
+                        return Ok(char_first);
+                    }
+                }
+            }
+        }
+    }
+
+    Err(eyre!(
+        "Not common character found in 3 rucksacks: '{}',  '{}' and '{}'",
+        first,
+        second,
+        third,
     ))
 }
 
@@ -79,11 +127,43 @@ mod tests {
     }
 
     #[test]
-    fn test_get_rucksack_items() {
+    fn test_get_rucksack_items_part1() {
         let input_str = "vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\nPmmdzqPrVvPwwTWBwg\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\nttgJtRGJQctTZtZT\nCrZsJsPPZsGzwwsLwLmpwMDw\n";
         let expected_result: Vec<char> = vec!['p', 'L', 'P', 'v', 't', 's'];
 
-        let output_list = get_rucksack_items(input_str).unwrap();
+        let output_list = get_rucksack_items_part1(input_str).unwrap();
+
+        assert_eq!(output_list.len(), expected_result.len());
+
+        let matching = output_list
+            .iter()
+            .zip(&expected_result)
+            .filter(|&(a, b)| a.eq(b))
+            .count();
+
+        assert_eq!(matching, expected_result.len());
+    }
+
+    #[rstest]
+    #[case("abcd", "xxax", "dxxa", 'a')]
+    fn test_get_common_item_in_3_rucksacks(
+        #[case] first: &str,
+        #[case] second: &str,
+        #[case] third: &str,
+        #[case] expected: char,
+    ) {
+        assert_eq!(
+            get_common_item_in_3_rucksacks(first, second, third).unwrap(),
+            expected
+        );
+    }
+
+    #[test]
+    fn test_get_rucksack_items_part2() {
+        let input_str = "vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\nPmmdzqPrVvPwwTWBwg\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\nttgJtRGJQctTZtZT\nCrZsJsPPZsGzwwsLwLmpwMDw\n";
+        let expected_result: Vec<char> = vec!['r', 'Z'];
+
+        let output_list = get_rucksack_items_part2(input_str).unwrap();
 
         assert_eq!(output_list.len(), expected_result.len());
 
