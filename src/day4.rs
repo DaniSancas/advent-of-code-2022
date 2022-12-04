f008aa958c8cd861ade6fca0dee751bca2c15de4
Dani Sancas <lord.sancas@gmail.com>
2022-12-04 22:31:10 +0100

    day4 part2

--- src/day4.rs
+++ src/day4.rs
@@ -2,31 +2,62 @@ use aoc_runner_derive::aoc;
 use eyre::Result;
 use regex::{Captures, Regex};
 
-#[aoc(day4, part1)]
-pub fn part1(input: &str) -> u32 {
-    compile_regex(input).unwrap_or_default()
+struct Ranges {
+    left_start: u32,
+    left_end: u32,
+    right_start: u32,
+    right_end: u32,
 }
 
-fn compile_regex(input: &str) -> Result<u32> {
+#[aoc(day4, part1)]
+pub fn part1(input: &str) -> u32 {
+    compile_regex(input, get_ranges_fully_contained).unwrap_or_default()
+}
+
+#[aoc(day4, part2)]
+pub fn part2(input: &str) -> u32 {
+    compile_regex(input, get_ranges_partially_contained).unwrap_or_default()
+}
+
+fn compile_regex(input: &str, ranges_contained_function: fn(Result<Ranges>) -> Result<u32>) -> Result<u32> {
     let regex = Regex::new(r"(\d+)-(\d+),(\d+)-(\d+)")?;
     let total_ranges = regex
         .captures_iter(input)
         .into_iter()
-        .map(|group| get_ranges_fully_contained(group))
+        .map(|group| group_to_ranges(&group))
+        .map(ranges_contained_function)
         .map(|group| group.unwrap_or_default())
         .sum();
 
     Ok(total_ranges)
 }
 
-fn get_ranges_fully_contained(group: Captures) -> Result<u32> {
-    let left_start = group[1].parse::<u32>()?;
-    let left_end = group[2].parse::<u32>()?;
-    let right_start = group[3].parse::<u32>()?;
-    let right_end = group[4].parse::<u32>()?;
+fn group_to_ranges(group: &Captures) -> Result<Ranges> {
+    Ok(Ranges {
+        left_start: group[1].parse::<u32>()?,
+        left_end: group[2].parse::<u32>()?,
+        right_start: group[3].parse::<u32>()?,
+        right_end: group[4].parse::<u32>()?,
+    })
+}
 
-    if (left_start <= right_start && left_end >= right_end)
-        || (right_start <= left_start && right_end >= left_end)
+fn get_ranges_fully_contained(ranges_maybe: Result<Ranges>) -> Result<u32> {
+    let ranges = ranges_maybe?;
+
+    if (ranges.left_start <= ranges.right_start && ranges.left_end >= ranges.right_end)
+        || (ranges.right_start <= ranges.left_start && ranges.right_end >= ranges.left_end)
+    {
+        Ok(1)
+    } else {
+        Ok(0)
+    }
+}
+
+fn get_ranges_partially_contained(ranges_maybe: Result<Ranges>) -> Result<u32> {
+    let ranges = ranges_maybe?;
+
+    if (ranges.left_start <= ranges.right_start && ranges.left_end >= ranges.right_start)
+        || (ranges.right_start <= ranges.left_start && ranges.right_end >= ranges.left_start)
     {
         Ok(1)
     } else {
@@ -37,6 +68,7 @@ fn get_ranges_fully_contained(group: Captures) -> Result<u32> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rstest::rstest;
 
     #[test]
     fn test_part1() {
@@ -47,5 +79,55 @@ mod tests {
         let output = part1(input);
 
         assert_eq!(output, expected);
+    }
+
+    #[rstest]
+    #[case(2, 4, 6, 8, 0)]
+    #[case(2, 8, 3, 7, 1)]
+    #[case(5, 10, 6, 9, 1)]
+    fn test_get_ranges_fully_contained(
+        #[case] ls: u32,
+        #[case] le: u32,
+        #[case] rs: u32,
+        #[case] re: u32,
+        #[case] expected: u32,
+    ) {
+        let input: Result<Ranges> = Ok(Ranges {
+            left_start: ls,
+            left_end: le,
+            right_start: rs,
+            right_end: re,
+        });
+
+        let output = get_ranges_fully_contained(input);
+
+        assert_eq!(output.unwrap(), expected);
+    }
+
+    #[rstest]
+    #[case(2, 4, 6, 8, 0)]
+    #[case(2, 4, 5, 7, 0)]
+    #[case(5, 7, 7, 9, 1)]
+    #[case(2, 8, 3, 7, 1)]
+    #[case(6, 7, 4, 6, 1)]
+    #[case(2, 6, 4, 8, 1)]
+    #[case(5, 10, 6, 9, 1)]
+    fn test_get_ranges_partially_contained(
+        #[case] ls: u32,
+        #[case] le: u32,
+        #[case] rs: u32,
+        #[case] re: u32,
+        #[case] expected: u32,
+    ) {
+        let input: Result<Ranges> = Ok(Ranges {
+            left_start: ls,
+            left_end: le,
+            right_start: rs,
+            right_end: re,
+        });
+
+        let output = get_ranges_partially_contained(input);
+
+        assert_eq!(output.unwrap(), expected);
     }
 }
