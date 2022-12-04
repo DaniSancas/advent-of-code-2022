4b3c243dd6aa9d64ccdd8b6a3ce2c8e4e74842ab
Dani Sancas <lord.sancas@gmail.com>
2022-12-04 19:25:35 +0100

    day2 part2

--- src/day2.rs
+++ src/day2.rs
@@ -7,7 +7,19 @@ pub fn part1(input: &str) -> u32 {
     for line in input.lines() {
         let theirs = line.chars().nth(0);
         let mine = line.chars().nth(2);
-        total_score += get_round_result(theirs, mine);
+        total_score += get_round_result_part1(theirs, mine);
+    }
+
+    total_score
+}
+
+#[aoc(day2, part2)]
+pub fn part2(input: &str) -> u32 {
+    let mut total_score: u32 = 0;
+    for line in input.lines() {
+        let theirs = line.chars().nth(0);
+        let mine = line.chars().nth(2);
+        total_score += get_round_result_part2(theirs, mine);
     }
 
     total_score
@@ -28,7 +40,7 @@ pub fn part1(input: &str) -> u32 {
 /// Example:
 ///
 /// A vs Y results in a Win (6) + Paper selected (2) = 8 total points
-fn get_round_result(theirs: Option<char>, mine: Option<char>) -> u32 {
+fn get_round_result_part1(theirs: Option<char>, mine: Option<char>) -> u32 {
     match (theirs, mine) {
         (Some(t), Some(m)) => match (t, m) {
             ('A', 'X') => 3 + 1, // Rock vs Rock
@@ -46,6 +58,51 @@ fn get_round_result(theirs: Option<char>, mine: Option<char>) -> u32 {
     }
 }
 
+/// Retireves an opponent choice (left) and the strategy to follow (right).
+///
+/// Left:
+/// A - Rock
+/// B - Paper
+/// C - Scissors
+///
+/// Right:
+/// X - Loose
+/// Y - Draw
+/// Z - Win
+///
+/// Resolves a round (left => them, right => me) and sums the score in two steps:
+///
+/// First step, points agains opponent:
+/// - Win: 6
+/// - Draw: 3
+/// - Loss: 0
+///
+/// Second step, points for choosing an option:
+/// - Rock: 1
+/// - Paper: 2
+/// - Scissors: 3
+///
+/// Example:
+///
+/// A vs Y results in a Win (6) + Paper selected (2) = 8 total points
+fn get_round_result_part2(theirs: Option<char>, mine: Option<char>) -> u32 {
+    match (theirs, mine) {
+        (Some(t), Some(m)) => match (t, m) {
+            ('A', 'X') => 0 + 3, // Loose against Rock (Scissors)
+            ('A', 'Y') => 3 + 1, // Draw against Rock (Rock)
+            ('A', 'Z') => 6 + 2, // Win against Rock (Paper)
+            ('B', 'X') => 0 + 1, // Loose against Paper (Rock)
+            ('B', 'Y') => 3 + 2, // Draw against Paper (Paper)
+            ('B', 'Z') => 6 + 3, // Win against Paper (Scissors)
+            ('C', 'X') => 0 + 2, // Loose against Scissors (Paper)
+            ('C', 'Y') => 3 + 3, // Draw against Scissors (Scissors)
+            ('C', 'Z') => 6 + 1, // Win against Scissors (Rock)
+            (_, _) => 0,
+        },
+        (_, _) => 0,
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -56,20 +113,42 @@ mod tests {
     #[case(Some('A'), Some('Y'), 8)]
     #[case(Some('B'), Some('X'), 1)]
     #[case(Some('C'), Some('Z'), 6)]
-    fn test_get_round_result(
+    fn test_get_round_result_part1(
         #[case] theirs: Option<char>,
         #[case] mine: Option<char>,
         #[case] expected: u32,
     ) {
-        assert_eq!(get_round_result(theirs, mine), expected);
+        assert_eq!(get_round_result_part1(theirs, mine), expected);
     }
 
     #[test]
-    fn test_get_total_score() {
+    fn test_get_total_score_part1() {
         let input_str = "A Y\nB X\nC Z\n";
 
         let expected_result: u32 = 15;
 
         assert_eq!(part1(input_str), expected_result);
+    }
+
+    #[rstest]
+    #[case(None, None, 0)]
+    #[case(Some('A'), Some('Y'), 4)]
+    #[case(Some('B'), Some('X'), 1)]
+    #[case(Some('C'), Some('Z'), 7)]
+    fn test_get_round_result_part2(
+        #[case] theirs: Option<char>,
+        #[case] mine: Option<char>,
+        #[case] expected: u32,
+    ) {
+        assert_eq!(get_round_result_part2(theirs, mine), expected);
+    }
+
+    #[test]
+    fn test_get_total_score_part2() {
+        let input_str = "A Y\nB X\nC Z\n";
+
+        let expected_result: u32 = 12;
+
+        assert_eq!(part2(input_str), expected_result);
     }
 }
